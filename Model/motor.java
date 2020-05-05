7c930025ec123280da7cf18ec3a40dcbd0a171c2
Mohamedhany99 <50308768+Mohamedhany99@users.noreply.github.com>
2020-05-05 23:36:13 +0200

    Add files via upload

---
+++ Model/motor.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author aa
+ */
+public class motor {
+    private boolean state;
+    public motor(boolean st)
+    {
+        this.state=st;
+    }
+    public boolean getmotorstate()
+    {
+        return state;
+    }
+    public void elevator_down()
+    {
+
+    }
+    public void elevator_up()
+    {
+
+    }
+}
---
+++ Model/powersource.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author aa
+ */
+public class powersource {
+    private boolean state;
+    public powersource(boolean st)
+    {
+        this.state=st;
+    }
+    public boolean getstate()
+    {
+        return state;
+    }
+}
